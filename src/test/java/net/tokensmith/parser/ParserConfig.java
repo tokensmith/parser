f870ca78be203b15dde1e1bc8ee651fa8032b809
= <tom.s.mackenzie@gmail.com>
2020-02-27 19:09:08 -0600

    10: constructor to add custom builders

--- src/test/java/net/tokensmith/parser/ParserConfig.java
+++ src/test/java/net/tokensmith/parser/ParserConfig.java
@@ -14,26 +14,45 @@ import java.util.function.Function;
 
 public class ParserConfig {
 
-    public ReflectParameter reflectParameter() {
+    public Map<String, Function<String, Object>> builders() {
         Map<String, Function<String, Object>> builders = new HashMap<>();
         builders.put("java.util.UUID", s -> {
             try{
                 return UUID.fromString(s);
             } catch (Exception e) {
-                throw new ConstructException("", e, s);
+                throw new ConstructException("Unable to construct UUID", e, s);
             }
         });
+        return builders;
+    }
+
+    public ReflectParameter reflectParameter(Map<String, Function<String, Object>> builders) {
         return new ReflectParameter(builders);
     }
 
     public Parser parser() {
+        Map<String, Function<String, Object>> builders = builders();
         return new Parser(
-            reflectParameter(),
+            reflectParameter(builders),
             new GraphTranslator(),
             new OptionalParam(),
             new RequiredParam(),
             new TypeParserFactory(),
             new NestedTypeSetterFactory()
+        );
+    }
+
+    public Parser parser(Map<String, Function<String, Object>> builders) {
+        Map<String, Function<String, Object>> defaultBuilders = builders();
+        defaultBuilders.putAll(builders);
+
+        return new Parser(
+                reflectParameter(defaultBuilders),
+                new GraphTranslator(),
+                new OptionalParam(),
+                new RequiredParam(),
+                new TypeParserFactory(),
+                new NestedTypeSetterFactory()
         );
     }
 }
